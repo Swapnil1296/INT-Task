8ee1dc14d51c6db8a9fba13ea4626f53b44e67a4
Swapnil1296 <swapnillandage79@gmail.com>
2022-09-07 16:18:57 +0530

    json file added

--- task/src/components/cards/UiCards.jsx
+++ task/src/components/cards/UiCards.jsx
@@ -1,20 +1,46 @@
 import React from 'react'
-import {Button, Col, Container, Row, Stack} from 'react-bootstrap'
+import {Button, Card, Col, Container, Row, Stack, Table} from 'react-bootstrap'
 const UiCards = () => {
   return (
     <>
-<Container className='justify-content-md-center  border'>
-   <Row className="justify-content-md-center ">
-    <Col className='text-center' ><h3>First Terminal Examination 2022-23</h3></Col>
-    </Row> 
-    <Row className="justify-content-md-center ">
-    <Col className='text-center' ><h5>Academic Performance</h5></Col>
-    </Row> 
-   <div className="d-flex justify-content-center " >
-   <div></div>
-   <div></div>
+<Container className='container fluid'>
+<Row className='m-2 '>
+    <Col className=' text-center border' ><h4>First Examination 29</h4></Col>
+</Row>
+<Row className='m-2'>
+    <Col className=' text-center border'><h5>First Examination 29</h5></Col>
+</Row>
+<Row className='m-2'>
+    <Col className=' text-center border'><h5>First Examination 29</h5></Col>
+</Row>
 
-   </div>
+    <Row className='m-2 border'>
+        <Col  className=' text-center border'  xs lg="7">
+            <Row className='border'>
+
+            <h5 style={{color:"blue"}}>Part-I</h5>
+            </Row>
+           <Row>
+           <Col  className=' text-center border fw-bold'>SrNo</Col>
+           <Col  className=' text-center border fw-bold text-primary'>Subjects</Col>
+           <Col  className=' text-center border'>
+            <Row>
+                <Col>MAA</Col>
+                <Col>MAA</Col>
+                <Col>MAA</Col>
+                <Col>MAA</Col>
+                <Col>MAA</Col>
+
+            </Row>
+           </Col>
+           <Col  className=' text-center border'>Name</Col>
+           </Row>
+   
+        </Col>
+        <Col className=' text-center border'><h5 style={{color:"blue"}}>Part-II</h5></Col>
+      </Row>
+
+   
 </Container>
     </>
     

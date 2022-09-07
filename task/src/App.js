be11018302b9738137e3034664c95d5a1d2b37cf
Swapnil1296 <swapnillandage79@gmail.com>
2022-09-07 14:17:41 +0530

    working

---
+++ task/src/App.js
@@ -0,0 +1,12 @@
+import UiCards from "./components/cards/UiCards";
+
+
+function App() {
+  return (
+    <div>
+  <UiCards/>
+    </div>
+  );
+}
+
+export default App;
---
+++ task/src/components/cards/UiCards.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Button, Col, Container, Row, Stack} from 'react-bootstrap'
+const UiCards = () => {
+  return (
+    <>
+<Container className='justify-content-md-center  border'>
+   <Row className="justify-content-md-center ">
+    <Col className='text-center' ><h3>First Terminal Examination 2022-23</h3></Col>
+    </Row> 
+    <Row className="justify-content-md-center ">
+    <Col className='text-center' ><h5>Academic Performance</h5></Col>
+    </Row> 
+   <div className="d-flex justify-content-center " >
+   <div></div>
+   <div></div>
+
+   </div>
+</Container>
+    </>
+    
+  )
+}
+
+export default UiCards
